04908871ed4273221a15fce2a684ffce9729204d
ShinDSR <danang.wibisono.ft20@mail.umy.ac.id>
2022-06-20 13:39:42 +0700

    Prayer Time

--- app/src/main/java/com/example/prayertime/DbContract.java
+++ app/src/main/java/com/example/prayertime/DbContract.java
@@ -1,6 +1,6 @@
 package com.example.prayertime;
 
 public class DbContract {
-    public static final String SERVER_LOGIN_URL = "http://192.168.0.117/prayer_time/checkLogin.php";
-    public static final String SERVER_REGISTER_URL = "http://192.168.0.117/prayer_time/createData.php";
+    public static final String SERVER_LOGIN_URL = "http://10.69.8.44/prayer_time/checkLogin.php";
+    public static final String SERVER_REGISTER_URL = "http://10.69.8.44/prayer_time/createData.php";
 }
